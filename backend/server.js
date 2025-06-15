eb53202d0c096be43ec02fbc2a861762fcafce8f
TechnoRishav <rishav2004.taral@gmail.com>
2025-06-15 11:21:13 +0530

    admin frontend url added

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 app.use(
     cors({
         origin: (origin, callback) => {
-            const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+            const allowedOrigins = ['https://foodiefrenzy-frontend.onrender.com', 'https://foodiefrenzy-admin.onrender.com/'];
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
