3d656b7138f8aefd1fe16185fc2bcfa116724d88
TechnoRishav <rishav2004.taral@gmail.com>
2025-06-19 19:09:59 +0530

    trying

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -32,13 +32,10 @@ export const createItem = async (req, res, next) => {
 
 export const getItems = async (_req, res, next) => {
     try {
-        const protocol =
-            process.env.NODE_ENV === 'production' ? 'https' : _req.protocol;
-        const host = `${protocol}://${_req.get('host')}`;
         const items = await Item.find().sort({ createdAt: -1 });
         const withFullUrl = items.map(i => ({
             ...i.toObject(),
-            imageUrl: i.imageUrl ? host + i.imageUrl : '',
+            imageUrl: i.imageUrl ? i.imageUrl : '',
         }));
         res.json(withFullUrl);
     } catch (err) {
